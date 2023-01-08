77f41988670f8d5fac8c54ed82de9d2b26d9776b
tecochard <theo.ecochard@eleves.ec-nantes.fr>
2023-01-08 18:12:03 +0100

    nettoyage scripts part3

--- Assets/Scripts/MusicChoice.cs
+++ Assets/Scripts/MusicChoice.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class MusicChoice : MonoBehaviour
 {
+    // Script to register the choice of music of the player
     public AudioSource easy;
     public AudioSource hard;
 
